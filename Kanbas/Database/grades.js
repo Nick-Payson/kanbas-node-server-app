5b8bc3e0c735ed0b364075cc85400ed2f70ae307
Nick Payson <38924251+Nick-Payson@users.noreply.github.com>
2024-08-05 15:52:18 -0400

    first commit

---
+++ Kanbas/Database/grades.js
@@ -0,0 +1,725 @@
+export default [
+    {
+      "_id": "1",
+      "student": "121",
+      "assignment": "A101",
+      "grade": "90"
+    },
+    {
+      "_id": "2",
+      "student": "122",
+      "assignment": "A101",
+      "grade": "91"
+    },
+    {
+      "_id": "3",
+      "student": "123",
+      "assignment": "A101",
+      "grade": "92"
+    },
+    {
+      "_id": "4",
+      "student": "121",
+      "assignment": "A102",
+      "grade": "92"
+    },
+    {
+      "_id": "5",
+      "student": "122",
+      "assignment": "A102",
+      "grade": "91"
+    },
+    {
+      "_id": "6",
+      "student": "123",
+      "assignment": "A102",
+      "grade": "90"
+    },
+  
+    {
+      "_id": "7",
+      "student": "124",
+      "assignment": "A201",
+      "grade": "80"
+    },
+    {
+      "_id": "8",
+      "student": "125",
+      "assignment": "A201",
+      "grade": "81"
+    },
+    {
+      "_id": "9",
+      "student": "126",
+      "assignment": "A201",
+      "grade": "82"
+    },
+  
+    {
+      "_id": "10",
+      "student": "124",
+      "assignment": "A202",
+      "grade": "82"
+    },
+    {
+      "_id": "11",
+      "student": "125",
+      "assignment": "A202",
+      "grade": "81"
+    },
+    {
+      "_id": "12",
+      "student": "126",
+      "assignment": "A202",
+      "grade": "80"
+    },
+    {
+      "_id": "13",
+      "student": "121",
+      "assignment": "A103",
+      "grade": "90"
+    },
+    {
+      "_id": "14",
+      "student": "122",
+      "assignment": "A103",
+      "grade": "100"
+    },
+    {
+      "_id": "15",
+      "student": "123",
+      "assignment": "A103",
+      "grade": "80"
+    },
+    {
+      "_id": "16",
+      "student": "124",
+      "assignment": "A203",
+      "grade": "100"
+    },
+    {
+      "_id": "17",
+      "student": "125",
+      "assignment": "A203",
+      "grade": "100"
+    },
+    {
+      "_id": "18",
+      "student": "126",
+      "assignment": "A203",
+      "grade": "80"
+    },
+    {
+      "_id": "19",
+      "student": "124",
+      "assignment": "A301",
+      "grade": "85"
+    },
+    {
+      "_id": "20",
+      "student": "124",
+      "assignment": "A302",
+      "grade": "90"
+    },
+    {
+      "_id": "21",
+      "student": "124",
+      "assignment": "A303",
+      "grade": "80"
+    },
+    {
+      "_id": "22",
+      "student": "121",
+      "assignment": "A301",
+      "grade": "40"
+    },
+    {
+      "_id": "23",
+      "student": "121",
+      "assignment": "A302",
+      "grade": "100"
+    },
+    {
+      "_id": "24",
+      "student": "121",
+      "assignment": "A303",
+      "grade": "100"
+    },
+    {
+        "_id": "25",
+        "student": "121",
+        "assignment": "A401",
+        "grade": "90"
+      },
+      {
+        "_id": "26",
+        "student": "122",
+        "assignment": "A401",
+        "grade": "91"
+      },
+      {
+        "_id": "27",
+        "student": "123",
+        "assignment": "A401",
+        "grade": "92"
+      },
+      {
+        "_id": "28",
+        "student": "121",
+        "assignment": "A402",
+        "grade": "90"
+      },
+      {
+        "_id": "29",
+        "student": "122",
+        "assignment": "A402",
+        "grade": "91"
+      },
+      {
+        "_id": "30",
+        "student": "123",
+        "assignment": "A402",
+        "grade": "92"
+      },
+      {
+        "_id": "31",
+        "student": "121",
+        "assignment": "A403",
+        "grade": "90"
+      },
+      {
+        "_id": "32",
+        "student": "122",
+        "assignment": "A403",
+        "grade": "91"
+      },
+      {
+        "_id": "33",
+        "student": "123",
+        "assignment": "A403",
+        "grade": "92"
+      },
+      {
+        "_id": "34",
+        "student": "124",
+        "assignment": "A503",
+        "grade": "100"
+      },
+      {
+        "_id": "35",
+        "student": "125",
+        "assignment": "A503",
+        "grade": "100"
+      },
+      {
+        "_id": "36",
+        "student": "126",
+        "assignment": "A503",
+        "grade": "80"
+      },
+      {
+        "_id": "37",
+        "student": "124",
+        "assignment": "A502",
+        "grade": "100"
+      },
+      {
+        "_id": "38",
+        "student": "125",
+        "assignment": "A502",
+        "grade": "100"
+      },
+      {
+        "_id": "39",
+        "student": "126",
+        "assignment": "A502",
+        "grade": "80"
+      },
+      {
+        "_id": "40",
+        "student": "124",
+        "assignment": "A501",
+        "grade": "100"
+      },
+      {
+        "_id": "41",
+        "student": "125",
+        "assignment": "A501",
+        "grade": "100"
+      },
+      {
+        "_id": "42",
+        "student": "126",
+        "assignment": "A501",
+        "grade": "80"
+      },
+      {
+        "_id": "43",
+        "student": "124",
+        "assignment": "A603",
+        "grade": "100"
+      },
+      {
+        "_id": "44",
+        "student": "121",
+        "assignment": "A603",
+        "grade": "100"
+      },
+      {
+        "_id": "45",
+        "student": "124",
+        "assignment": "A602",
+        "grade": "100"
+      },
+      {
+        "_id": "46",
+        "student": "121",
+        "assignment": "A602",
+        "grade": "99"
+      },
+      {
+        "_id": "47",
+        "student": "124",
+        "assignment": "A601",
+        "grade": "100"
+      },
+      {
+        "_id": "48",
+        "student": "121",
+        "assignment": "A601",
+        "grade": "100"
+      },
+      {
+        "_id": "49",
+        "student": "122",
+        "assignment": "A701",
+        "grade": "100"
+      },
+      {
+        "_id": "50",
+        "student": "121",
+        "assignment": "A701",
+        "grade": "100"
+      },
+      {
+        "_id": "51",
+        "student": "122",
+        "assignment": "A702",
+        "grade": "100"
+      },
+      {
+        "_id": "52",
+        "student": "121",
+        "assignment": "A702",
+        "grade": "100"
+      },
+      {
+        "_id": "53",
+        "student": "122",
+        "assignment": "A703",
+        "grade": "100"
+      },
+      {
+        "_id": "54",
+        "student": "121",
+        "assignment": "A703",
+        "grade": "100"
+      },
+      {
+        "_id": "55",
+        "student": "123",
+        "assignment": "A803",
+        "grade": "100"
+      },
+      {
+        "_id": "56",
+        "student": "123",
+        "assignment": "A802",
+        "grade": "100"
+      },
+      {
+        "_id": "57",
+        "student": "123",
+        "assignment": "A801",
+        "grade": "100"
+      },
+      {
+        "_id": "58",
+        "student": "125",
+        "assignment": "A901",
+        "grade": "100"
+      },
+      {
+        "_id": "59",
+        "student": "124",
+        "assignment": "A901",
+        "grade": "100"
+      },
+      {
+        "_id": "60",
+        "student": "121",
+        "assignment": "A901",
+        "grade": "100"
+      },
+      {
+        "_id": "61",
+        "student": "126",
+        "assignment": "A901",
+        "grade": "100"
+      },
+      {
+        "_id": "62",
+        "student": "125",
+        "assignment": "A903",
+        "grade": "100"
+      },
+      {
+        "_id": "63",
+        "student": "124",
+        "assignment": "A903",
+        "grade": "98"
+      },
+      {
+        "_id": "64",
+        "student": "121",
+        "assignment": "A903",
+        "grade": "97"
+      },
+      {
+        "_id": "65",
+        "student": "126",
+        "assignment": "A903",
+        "grade": "100"
+      },
+      {
+        "_id": "66",
+        "student": "125",
+        "assignment": "A902",
+        "grade": "78"
+      },
+      {
+        "_id": "67",
+        "student": "124",
+        "assignment": "A902",
+        "grade": "90"
+      },
+      {
+        "_id": "68",
+        "student": "121",
+        "assignment": "A902",
+        "grade": "100"
+      },
+      {
+        "_id": "69",
+        "student": "126",
+        "assignment": "A902",
+        "grade": "100"
+      },
+      {
+        "_id": "70",
+        "student": "124",
+        "assignment": "A1001",
+        "grade": "90"
+      },
+      {
+        "_id": "71",
+        "student": "124",
+        "assignment": "A1002",
+        "grade": "100"
+      },
+      {
+        "_id": "72",
+        "student": "124",
+        "assignment": "A1003",
+        "grade": "93"
+      },
+      {
+        "_id": "73",
+        "student": "121",
+        "assignment": "A1102",
+        "grade": "90"
+      },
+      {
+        "_id": "74",
+        "student": "121",
+        "assignment": "A1101",
+        "grade": "90"
+      },
+      {
+        "_id": "75",
+        "student": "121",
+        "assignment": "A1103",
+        "grade": "90"
+      },
+      {
+        "_id": "76",
+        "student": "122",
+        "assignment": "A1202",
+        "grade": "90"
+      },
+      {
+        "_id": "77",
+        "student": "123",
+        "assignment": "A1202",
+        "grade": "90"
+      },
+      {
+        "_id": "78",
+        "student": "124",
+        "assignment": "A1202",
+        "grade": "90"
+      },
+      {
+        "_id": "79",
+        "student": "125",
+        "assignment": "A1202",
+        "grade": "90"
+      },
+      {
+        "_id": "80",
+        "student": "126",
+        "assignment": "A1202",
+        "grade": "90"
+      },
+      {
+        "_id": "21",
+        "student": "122",
+        "assignment": "A1201",
+        "grade": "90"
+      },
+      {
+        "_id": "82",
+        "student": "123",
+        "assignment": "A1201",
+        "grade": "90"
+      },
+      {
+        "_id": "83",
+        "student": "124",
+        "assignment": "A1201",
+        "grade": "90"
+      },
+      {
+        "_id": "84",
+        "student": "125",
+        "assignment": "A1201",
+        "grade": "92"
+      },
+      {
+        "_id": "85",
+        "student": "126",
+        "assignment": "A1201",
+        "grade": "80"
+      },
+      {
+        "_id": "86",
+        "student": "122",
+        "assignment": "A1203",
+        "grade": "100"
+      },
+      {
+        "_id": "87",
+        "student": "123",
+        "assignment": "A1203",
+        "grade": "90"
+      },
+      {
+        "_id": "88",
+        "student": "124",
+        "assignment": "A1203",
+        "grade": "91"
+      },
+      {
+        "_id": "89",
+        "student": "125",
+        "assignment": "A1203",
+        "grade": "91"
+      },
+      {
+        "_id": "90",
+        "student": "126",
+        "assignment": "A1203",
+        "grade": "90"
+      },
+      {
+        "_id": "91",
+        "student": "121",
+        "assignment": "A1303",
+        "grade": "91"
+      },
+      {
+        "_id": "92",
+        "student": "121",
+        "assignment": "A1302",
+        "grade": "91"
+      },
+      {
+        "_id": "93",
+        "student": "121",
+        "assignment": "A1301",
+        "grade": "90"
+      },
+      {
+        "_id": "94",
+        "student": "124",
+        "assignment": "A1403",
+        "grade": "91"
+      },
+      {
+        "_id": "95",
+        "student": "124",
+        "assignment": "A1402",
+        "grade": "91"
+      },
+      {
+        "_id": "96",
+        "student": "124",
+        "assignment": "A1401",
+        "grade": "98"
+      },
+      {
+        "_id": "97",
+        "student": "121",
+        "assignment": "A1503",
+        "grade": "91"
+      },
+      {
+        "_id": "98",
+        "student": "121",
+        "assignment": "A1502",
+        "grade": "91"
+      },
+      {
+        "_id": "99",
+        "student": "121",
+        "assignment": "A1501",
+        "grade": "90"
+      },
+      {
+        "_id": "100",
+        "student": "123",
+        "assignment": "A1603",
+        "grade": "91"
+      },
+      {
+        "_id": "101",
+        "student": "123",
+        "assignment": "A1602",
+        "grade": "91"
+      },
+      {
+        "_id": "102",
+        "student": "123",
+        "assignment": "A1601",
+        "grade": "90"
+      },
+      {
+        "_id": "103",
+        "student": "122",
+        "assignment": "A1603",
+        "grade": "91"
+      },
+      {
+        "_id": "104",
+        "student": "122",
+        "assignment": "A1602",
+        "grade": "91"
+      },
+      {
+        "_id": "105",
+        "student": "122",
+        "assignment": "A1601",
+        "grade": "90"
+      },
+      {
+        "_id": "106",
+        "student": "124",
+        "assignment": "A1703",
+        "grade": "91"
+      },
+      {
+        "_id": "107",
+        "student": "124",
+        "assignment": "A1702",
+        "grade": "97"
+      },
+      {
+        "_id": "108",
+        "student": "124",
+        "assignment": "A1701",
+        "grade": "98"
+      },
+      {
+        "_id": "109",
+        "student": "125",
+        "assignment": "A1803",
+        "grade": "91"
+      },
+      {
+        "_id": "110",
+        "student": "125",
+        "assignment": "A1802",
+        "grade": "97"
+      },
+      {
+        "_id": "111",
+        "student": "125",
+        "assignment": "A1801",
+        "grade": "98"
+      },
+      {
+        "_id": "112",
+        "student": "126",
+        "assignment": "A1903",
+        "grade": "95"
+      },
+      {
+        "_id": "113",
+        "student": "126",
+        "assignment": "A1902",
+        "grade": "97"
+      },
+      {
+        "_id": "114",
+        "student": "126",
+        "assignment": "A1901",
+        "grade": "98"
+      },
+      {
+        "_id": "115",
+        "student": "124",
+        "assignment": "A2003",
+        "grade": "91"
+      },
+      {
+        "_id": "116",
+        "student": "124",
+        "assignment": "A2002",
+        "grade": "97"
+      },
+      {
+        "_id": "117",
+        "student": "124",
+        "assignment": "A2001",
+        "grade": "98"
+      },
+      {
+        "_id": "118",
+        "student": "121",
+        "assignment": "A2103",
+        "grade": "91"
+      },
+      {
+        "_id": "119",
+        "student": "121",
+        "assignment": "A2102",
+        "grade": "97"
+      },
+      {
+        "_id": "120",
+        "student": "121",
+        "assignment": "A2101",
+        "grade": "98"
+      }
+      
+  ];
---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,442 @@
+export default [
+    {
+      "_id": "M101",
+      "name": "Introduction to Rocket Propulsion",
+      "description": "Basic principles of rocket propulsion and rocket engines.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "History of Rocketry",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Rocket Propulsion Fundamentals",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Rocket Engine Types",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M102",
+      "name": "Fuel and Combustion",
+      "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Rocket Fuel",
+          "description": "Overview of different types of rocket fuels.",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Combustion Processes",
+          "description": "Understanding combustion processes and efficiency.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Combustion Instability",
+          "description": "Understanding combustion instability and mitigation.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M103",
+      "name": "Nozzle Design",
+      "description": "Principles of rocket nozzle design and performance optimization.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Nozzle Design",
+          "description": "Overview of different types of rocket nozzles.",
+          "module": "M103"
+        },
+        {
+          "_id": "L302",
+          "name": "Nozzle Performance",
+          "description": "Understanding nozzle performance and efficiency.",
+          "module": "M103"
+        },
+        {
+          "_id": "L303",
+          "name": "Nozzle Optimization",
+          "description": "Optimizing nozzle design for specific applications.",
+          "module": "M103"
+        }
+      ]
+    },
+    {
+      "_id": "M201",
+      "name": "Fundamentals of Aerodynamics",
+      "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+      "course": "RS102"
+    },
+    {
+      "_id": "M202",
+      "name": "Subsonic and Supersonic Flow",
+      "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+      "course": "RS102"
+    },
+    {
+      "_id": "M203",
+      "name": "Aerodynamic Heating",
+      "description": "Study of aerodynamic heating and thermal protection systems.",
+      "course": "RS102"
+    },
+    {
+      "_id": "M301",
+      "name": "Spacecraft Structural Design",
+      "description": "Fundamentals of designing spacecraft structures and materials selection.",
+      "course": "RS103"
+    },
+    {
+      "_id": "M302",
+      "name": "Orbital Mechanics",
+      "description": "Understanding orbital dynamics and mission planning.",
+      "course": "RS103"
+    },
+    {
+      "_id": "M303",
+      "name": "Spacecraft Systems Engineering",
+      "description": "Overview of spacecraft systems and subsystems engineering.",
+      "course": "RS103"
+    },
+    {
+      "_id": "M401",
+      "name": "What is Chemistry",
+      "description": "Into to Organic Chemistry .",
+      "course": "RS104"
+    },
+    {
+      "_id": "M402",
+      "name": "What are those hexagons",
+      "description": "Understanding the hexagons and what they mean.",
+      "course": "RS104"
+    },
+    {
+      "_id": "M403",
+      "name": "What are the squiggly lines?",
+      "description": "Overview of organic chem notation.",
+      "course": "RS104"
+    },
+    {
+      "_id": "M501",
+      "name": "Intro to inorganic chem",
+      "description": "Intro to inorganic chem.",
+      "course": "RS105"
+    },
+    {
+      "_id": "M502",
+      "name": "Lab Basics",
+      "description": "How to do the labs and not blow anything up.",
+      "course": "RS105"
+    },
+    {
+      "_id": "M503",
+      "name": "Exo- and Endo-thermic reactions",
+      "description": "Chemicals go boom.",
+      "course": "RS105"
+    },
+    {
+      "_id": "M601",
+      "name": "Intro to physical Chem",
+      "description": "Intro to physical chemistry.",
+      "course": "RS106"
+    },
+    {
+      "_id": "M602",
+      "name": "Physics of chemical interactions",
+      "description": "Low level introduction to mollecular-level physics.",
+      "course": "RS106"
+    },
+    {
+      "_id": "M603",
+      "name": "acids and bases",
+      "description": "Ph and its importance in chemistry.",
+      "course": "RS106"
+    },
+    {
+      "_id": "M701",
+      "name": "Goblin",
+      "description": "Fundamentals of speaking Goblin.",
+      "course": "RS107"
+    },
+    {
+      "_id": "M702",
+      "name": "Elvish",
+      "description": "Understanding Elvish.",
+      "course": "RS107"
+    },
+    {
+      "_id": "M703",
+      "name": "Orc",
+      "description": "Overview of Orc-speak.",
+      "course": "RS107"
+    },
+    {
+      "_id": "M801",
+      "name": "Orc-Goblin Treaties",
+      "description": "History of relations between orcs and goblins.",
+      "course": "RS108"
+    },
+    {
+      "_id": "M802",
+      "name": "Human-Elf Treaties",
+      "description": "Understanding the human-elf dynamic.",
+      "course": "RS108"
+    },
+    {
+      "_id": "M803",
+      "name": "Hobbit Isolationism",
+      "description": "Overview of the history of Hobbits.",
+      "course": "RS108"
+    },
+    {
+      "_id": "M901",
+      "name": "Intro to middle earth magic",
+      "description": "Overview.",
+      "course": "RS109"
+    },
+    {
+      "_id": "M902",
+      "name": "Gandalf",
+      "description": "Understanding Gandalf.",
+      "course": "RS109"
+    },
+    {
+      "_id": "M903",
+      "name": "Saruman",
+      "description": "Understanding Saruman.",
+      "course": "RS109"
+    },
+    {
+      "_id": "1001",
+      "name": "Intro to Education",
+      "description": "Fundamentals of education.",
+      "course": "RS110"
+    },
+    {
+      "_id": "M1002",
+      "name": "Attention Span Limitations",
+      "description": "Understanding how to deal with people's short attention spans.",
+      "course": "RS110"
+    },
+    {
+      "_id": "M1003",
+      "name": "How to make a lecture boring",
+      "description": "A true teacher will ace this without studying at all.",
+      "course": "RS110"
+    },
+    {
+      "_id": "1101",
+      "name": "Intro to Freedom",
+      "description": "Fundamentals of Freedom.",
+      "course": "RS111"
+    },
+    {
+      "_id": "M1102",
+      "name": "Bald Eagles",
+      "description": "Birds.",
+      "course": "RS111"
+    },
+    {
+      "_id": "M1103",
+      "name": "Old paper documents",
+      "description": "Study old pieces of paper.",
+      "course": "RS111"
+    },
+    {
+      "_id": "1201",
+      "name": "Intro to 19th Century Education",
+      "description": "Fundamentals of education.",
+      "course": "RS112"
+    },
+    {
+      "_id": "M1202",
+      "name": "Old teaching methods",
+      "description": "Hitting students not recommended anymore.",
+      "course": "RS112"
+    },
+    {
+      "_id": "M1203",
+      "name": "Old teaching methods part 2",
+      "description": "Is the Dunce cap REALLY out of date?",
+      "course": "RS112"
+    },
+    {
+      "_id": "1301",
+      "name": "Intro to Christian Ethics",
+      "description": "Love thy neighbor.",
+      "course": "RS113"
+    },
+    {
+      "_id": "M1302",
+      "name": "Teachings of John",
+      "description": "Understanding John",
+      "course": "RS113"
+    },
+    {
+      "_id": "M1303",
+      "name": "Modern interpretations",
+      "description": "Blasphemy",
+      "course": "RS113"
+    },
+    {
+      "_id": "1401",
+      "name": "Intro to Jesus",
+      "description": "Turning water into wine",
+      "course": "RS114"
+    },
+    {
+      "_id": "M1402",
+      "name": "Christianity in the middle ages",
+      "description": "Crusades are bad.",
+      "course": "RS114"
+    },
+    {
+      "_id": "M1403",
+      "name": "Apostles and Missionaries",
+      "description": "A History of religious speakers",
+      "course": "RS114"
+    },
+    {
+      "_id": "1501",
+      "name": "Intro to Christian Leadership",
+      "description": "Preists",
+      "course": "RS115"
+    },
+    {
+      "_id": "M1502",
+      "name": "Intro to Christian Leadership part 2",
+      "description": "The Pope.",
+      "course": "RS115"
+    },
+    {
+      "_id": "M1503",
+      "name": "Intro to Christian service",
+      "description": "Be nice to others.",
+      "course": "RS115"
+    },
+    {
+      "_id": "1601",
+      "name": "Intro to Sufism",
+      "description": "Intro.",
+      "course": "RS116"
+    },
+    {
+      "_id": "M1602",
+      "name": "Unification with Allah",
+      "description": "The main goal.",
+      "course": "RS116"
+    },
+    {
+      "_id": "M1603",
+      "name": "Sufi traditions",
+      "description": "Traditions and Sufi culture",
+      "course": "RS116"
+    },
+    {
+      "_id": "1701",
+      "name": "Intro to Islamic Ethics",
+      "description": "Intro.",
+      "course": "RS117"
+    },
+    {
+      "_id": "M1702",
+      "name": "Virtuous deeds",
+      "description": "A core component of Islam",
+      "course": "RS117"
+    },
+    {
+      "_id": "M1703",
+      "name": "Fairness",
+      "description": "A core teaching of Islam",
+      "course": "RS117"
+    },
+    {
+      "_id": "1801",
+      "name": "Intro to Islam",
+      "description": "Basics of the religion.",
+      "course": "RS118"
+    },
+    {
+      "_id": "M1802",
+      "name": "HIdden Ideas in texts",
+      "description": "Understanding how to read between the lines.",
+      "course": "RS118"
+    },
+    {
+      "_id": "M1803",
+      "name": "Project",
+      "description": "Find your own hidden meanings.",
+      "course": "RS118"
+    },
+    {
+      "_id": "1901",
+      "name": "Intro to Ancient Chinese Environmental Management",
+      "description": "Intro.",
+      "course": "RS119"
+    },
+    {
+      "_id": "M1902",
+      "name": "Irrigation",
+      "description": "Irrigation in ancient China.",
+      "course": "RS119"
+    },
+    {
+      "_id": "M1903",
+      "name": "Resource Management",
+      "description": "Early Environmentalism in ancient China.",
+      "course": "RS119"
+    },
+    {
+      "_id": "2001",
+      "name": "Intro to Chinese Mythology",
+      "description": "Fundamentals of Chinese Mythology.",
+      "course": "RS120"
+    },
+    {
+      "_id": "M2002",
+      "name": "Chinese Leaders' Mythological Inspirations",
+      "description": "Intro",
+      "course": "RS120"
+    },
+    {
+      "_id": "M2003",
+      "name": "Chinese Leaders' Historical Inspirations",
+      "description": "Intro.",
+      "course": "RS120"
+    },
+    {
+      "_id": "2101",
+      "name": "Intro to Ancient Hydraulic Engineering",
+      "description": "Intro.",
+      "course": "RS121"
+    },
+    {
+      "_id": "M2102",
+      "name": "Greek/Roman accomplishments",
+      "description": "Intro",
+      "course": "RS121"
+    },
+    {
+      "_id": "M2103",
+      "name": "Babylonian accomplishments",
+      "description": "Intro.",
+      "course": "RS121"
+    }
+  
+  ];
+  
