9ac80555d1a5b38716dfb297a4639bf77300433e
Nick Payson <38924251+Nick-Payson@users.noreply.github.com>
2024-08-10 01:09:17 -0400

    a6

--- App.js
+++ App.js
@@ -5,9 +5,16 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import mongoose from 'mongoose';
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json()); //work AFTER this line 
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
